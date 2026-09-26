cdccaeffdd10dcf690a9b59c4cf95c03328a5be8
davisouza19 <davisouza.nb@gmail.com>
2026-09-25 21:57:03 -0300

    aula 11 de JS

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+//estrutura condicional simples:
+let vel = 60
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){
+    console.log('Multado!')
+}
+console.log(`Boa viajem`)
---
+++ aula11/ex009.js
@@ -0,0 +1,11 @@
+//estrutura condicional composta
+let pais = 'Brasil'
+let lugar
+
+if (pais == 'Brasil'){
+    lugar = 'Brasileiro'
+}else{
+    lugar = 'Estrangeiro'
+}
+
+console.log(`Você mora no ${pais} e é ${lugar}`)
